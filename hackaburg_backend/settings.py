723a664402d59b85072dea705da1d341b6cbe294
raspitim <raspitim@users.noreply.github.com>
2023-05-26 22:05:25 +0200

    debug true

--- hackaburg_backend/settings.py
+++ hackaburg_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xmtp$oo7c#134pqz!q-7t)p^(yc9y7)32eg66lltnjcwr%6v%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
