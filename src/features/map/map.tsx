a4bc84621d4d50dd360d8b49e620211477f77807
sdoxl <g.lstuma@gmail.com>
2023-05-26 13:38:16 +0200

    :)

--- src/features/map/map.tsx
+++ src/features/map/map.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import AppendHead from 'react-append-head';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, {LngLat, LngLatLike} from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 const queryClient = new QueryClient()
@@ -9,38 +9,82 @@ const queryClient = new QueryClient()
 const fetch_hubs = () => {
     return []
 }
-fetch_hubs()
 
-const get_location = async () => {
-    let loc = [0, 0]
-    await new Promise((res) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            loc = [position.coords.latitude, position.coords.longitude]
-            console.log("ll" + loc)
-            res()
-        })
-    })
-    return loc
-}
+const get_location = () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const loc = [position.coords.longitude, position.coords.latitude];
+                resolve(loc);
+                },
+            (error) => {
+                reject(error);
+            }
+        );
+    });
+};
 
 export const Map = () => {
-    const [location, setLocation] = useState([0, 0])
+
     useEffect(() => {
-        const func = async () => {
-            setLocation(await get_location())
-        }
-        func()
-    }, [])
+    const func = async () => {
+    try {
+      const loc = await get_location();
+
+      // Render map
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: loc,
+        zoom: 11
+      });
+
+      // Rest of your map-related code...
+    } catch (error) {
+            console.error('Error retrieving location:', error);
+    }
+
+        map.on("load", () => {
+            map.loadImage(
+                "https://docs.mapbox.com/mapbox-gl-js/assets/cat.png",
+                (error, image)  => {
+                    if (error) throw error;
+                    map.addImage('cat', image);
+                }
+            )
+
+            map.addSource('point', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': [
+                            {
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': loc
+                            }
+                        }
+                    ]
+                }
+            });
+
+            // Add a layer to use the image to represent the data.
+            map.addLayer({
+                'id': 'points',
+                'type': 'symbol',
+                'source': 'point', // reference the data source
+                'layout': {
+                    'icon-image': 'cat', // reference the image
+                'icon-size': 0.25
+                }
+            });
+    })
+  };
+
+  func();
+  }, []);
     mapboxgl.accessToken = 'pk.eyJ1IjoibHN0dW1hIiwiYSI6ImNsaTN0dnc3ZDBpMTkzZW1seml3NTZobDUifQ._Fo8j6VzHhLj-BDC5EW_xg';
-    setTimeout(() => {const map: mapboxgl.Map = new mapboxgl.Map({
-        container: 'map', // container ID
-        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: location, // starting position [lng, lat]
-        zoom: 9
-    })}, 50);
-
-
 
     return (
         <div style={{display: "grid"}}>
@@ -50,5 +94,5 @@ export const Map = () => {
                 <script src="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js" defer></script>
             </AppendHead>
         </div>
-        )
+    )
 }
--- src/features/passengerFlow/PassengerFlow.tsx
+++ src/features/passengerFlow/PassengerFlow.tsx
@@ -15,20 +15,6 @@ export const PassengerFlow = () => {
 	return (
 		<div>
 			<Map />
-			{step === "rideDetails" && (
-				<button onClick={() => setStep("drivers")}>Enter destination and when you ready to departure</button>
-			)}
-			{step === "drivers" && (
-				<button onClick={() => setStep("ride")}>Request a ride</button>
-			)}
-			{step === "ride" && (
-				<iframe src="https://giphy.com/embed/5bo92jPBIWKtHUump7" width="340" height="480" frameBorder="0"
-					className="giphy-embed"
-					allowFullScreen></iframe>
-			)}
-			{step === "final" && (
-				<div>You arrived!</div>
-			)}
 		</div>
 	)
 }
