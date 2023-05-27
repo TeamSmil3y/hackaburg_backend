2505033f9c62ad9b10dfa0a020026f328c2dc52d
sdoxl <g.lstuma@gmail.com>
2023-05-27 14:39:59 +0200

    hotfix update 1

--- main/update_manager.py
+++ main/update_manager.py
@@ -2,24 +2,24 @@ events = dict()
 
 def need_user(func):
     def wrapper(user, *args, **kwargs):
-        if user not in events:
-            events[user] = []
+        if user.id not in events:
+            events[user.id] = []
         func(user=user, *args, **kwargs)
     return wrapper
 
 def flush(user):
-    events[user] = []
+    events[user.id] = []
     
 def delete(user):
-    events.pop(user)
+    events.pop(user.id)
 
 @need_user
 def push_event(user, event):
-    events[user].append(event)
+    events[user.id].append(event)
 
 @need_user
 def request_update(user):
-    update = events[user].copy()
-    events[user] = []
+    update = events[user.id].copy()
+    events[user.id] = []
     
     return update
